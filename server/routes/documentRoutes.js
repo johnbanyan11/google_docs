cd4167a781fcce3f9af013d6bbb508cc13b5164f
johnbanyan11 <johnbanyan11@gmail.com>
2025-06-01 16:43:27 +0530

    collaborators api

--- server/routes/documentRoutes.js
+++ server/routes/documentRoutes.js
@@ -1,18 +1,19 @@
-const express = require('express');
+const express = require("express");
 const {
   createDocument,
   getDocument,
   getAllDocuments,
   updateDocument,
-  deleteDocument
-} = require('../controllers/documentController');
+  deleteDocument,
+} = require("../controllers/documentController");
 
 const router = express.Router();
 
-router.post('/', createDocument);            // Create
-router.get('/', getAllDocuments);            // Read all
-router.get('/:id', getDocument);             // Read one
-router.put('/:id', updateDocument);          // Update
-router.delete('/:id', deleteDocument);       // Delete
+router.post("/", createDocument); // Create
+router.get("/", getAllDocuments); // Read all
+router.get("/:id", getDocument); // Read one
+router.put("/:id", updateDocument); // Update
+router.delete("/:id", deleteDocument); // Delete
+router.put("/:docId/collaborators", addCollaborator); //addCollaborator
 
 module.exports = router;
