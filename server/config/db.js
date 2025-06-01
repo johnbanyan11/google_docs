93d8a6bd9d47826b863a1fed06fd2c7d468e105d
maneesh <maneesh.g@falconavl.com>
2025-06-01 16:06:56 +0530

    connection string added

--- server/config/db.js
+++ server/config/db.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/google-docs', {
+    await mongoose.connect('mongodb+srv://gonumaneesh:gonumaneesh@google-docs.hjpu2ic.mongodb.net/?retryWrites=true&w=majority&appName=google-docs', {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
     console.log('MongoDB connected');
-  } catch (err) {
+  } catch (err) {    
     console.error(err.message);
     process.exit(1);
   }
